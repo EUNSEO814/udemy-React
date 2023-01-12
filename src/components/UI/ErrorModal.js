de31ce3ff510bb46927e1f69b8482b68acd86207
EUNSEO814 <eunseo814@gmail.com>
2023-01-12 23:07:39 +0900

    ⚡️ section9-portal

--- src/components/UI/ErrorModal.js
+++ src/components/UI/ErrorModal.js
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 // import Card from "./Card";
 import Button from "./Button";
+import ReactDOM from "react-dom";
 
 const Card = styled.div`
   background: white;
@@ -41,7 +42,7 @@ const Actions = styled.footer`
   justify-content: flex-end;
 `;
 
-const Backdrop = styled.div`
+const BackdropStyle = styled.div`
   position: fixed;
   top: 0;
   left: 0;
@@ -50,40 +51,43 @@ const Backdrop = styled.div`
   z-index: 10;
   background: rgba(0, 0, 0, 0.75);
 `;
+
+const Backdrop = (props) => {
+  return <BackdropStyle onClick={props.onConfirm} />;
+};
+const Overlay = (props) => {
+  return (
+    <Card>
+      <Headers>
+        <H2>{props.title}</H2>
+      </Headers>
+      <Content>
+        <p>{props.message}</p>
+      </Content>
+      <Actions>
+        <Button type="button" text="Okey" onClick={props.onConfirm} />
+      </Actions>
+    </Card>
+  );
+};
+
 const ErrorModal = (props) => {
   return (
     <>
-      <Backdrop onClick={props.onConfirm} />
-      <Card>
-        <Headers>
-          <H2>{props.title}</H2>
-        </Headers>
-        <Content>
-          <p>{props.message}</p>
-        </Content>
-        <Actions>
-          <Button type="button" text="Okey" onClick={props.onConfirm} />
-        </Actions>
-      </Card>
+      {ReactDOM.createPortal(
+        <Backdrop onConfirm={props.onConfirm} />,
+        document.getElementById("backdrop-root")
+      )}
+      {ReactDOM.createPortal(
+        <Overlay
+          title={props.title}
+          message={props.message}
+          onConfirm={props.onConfirm}
+        />,
+        document.getElementById("overlay-root")
+      )}
     </>
   );
 };
 
 export default ErrorModal;
-
-// .backdrop {
-//     position: fixed;
-//     top: 0;
-//     left: 0;
-//     width: 100%;
-//     height: 100vh;
-//     z-index: 10;
-//     background: rgba(0, 0, 0, 0.75);
-//   }
-
-//   @media (min-width: 768px) {
-//     .modal {
-//       left: calc(50% - 20rem);
-//       width: 40rem;
-//     }
-//   }
