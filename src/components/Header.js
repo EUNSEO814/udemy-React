b85cea37794e644dc8f98522ddd387bb6d1e5d10
EUNSEO814 <eunseo814@gmail.com>
2023-02-03 17:37:20 +0900

    ⚡️ section18 redux-toolkit 다중슬라이스

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,22 +1,33 @@
-import classes from './Header.module.css';
+import classes from "./Header.module.css";
+import { useSelector, useDispatch } from "react-redux";
+import { authActions } from "../store";
 
 const Header = () => {
+  const dispatch = useDispatch();
+
+  const auth = useSelector((state) => state.auth.isAuthenticated);
+
+  const authHandler = () => {
+    dispatch(authActions.logout());
+  };
   return (
     <header className={classes.header}>
       <h1>Redux Auth</h1>
-      <nav>
-        <ul>
-          <li>
-            <a href='/'>My Products</a>
-          </li>
-          <li>
-            <a href='/'>My Sales</a>
-          </li>
-          <li>
-            <button>Logout</button>
-          </li>
-        </ul>
-      </nav>
+      {auth && (
+        <nav>
+          <ul>
+            <li>
+              <a href="/">My Products</a>
+            </li>
+            <li>
+              <a href="/">My Sales</a>
+            </li>
+            <li>
+              <button onClick={authHandler}>Logout</button>
+            </li>
+          </ul>
+        </nav>
+      )}
     </header>
   );
 };
