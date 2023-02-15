9c850805c2b2a4baddd4c8a014f7080984049f2d
EUNSEO814 <eunseo814@gmail.com>
2023-02-15 16:27:40 +0900

    ⚡️ 페이지 간 연결하기 (Link)

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "bee1e532898dddb88eee";
+/******/ 	var hotCurrentHash = "2e5b01c2719b13642110";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- pages/news/index.js
+++ pages/news/index.js
@@ -1,7 +1,21 @@
 // our domain.com/news
 
+import Link from "next/link";
+
 const NewsPage = () => {
-  return <h1>The News Page</h1>;
+  return (
+    <>
+      <h1>The News Page</h1>
+      <ul>
+        <li>
+          <Link href="/news/nextjs-is-a-great-framework">
+            NextJS is A Great Framework
+          </Link>
+        </li>
+        <li>Something Else</li>
+      </ul>
+    </>
+  );
 };
 
 export default NewsPage;
