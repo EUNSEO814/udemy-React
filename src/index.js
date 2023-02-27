7ce264b5c36ac65aa0fdd998aa4ff03049c4a954
EUNSEO814 <eunseo814@gmail.com>
2023-02-27 20:47:36 +0900

    ⚡️ 커스텀 훅 사용하기 useStore

--- src/index.js
+++ src/index.js
@@ -4,14 +4,13 @@ import { BrowserRouter } from "react-router-dom";
 
 import "./index.css";
 import App from "./App";
-import ProductsProvider from "./context/products-context";
+import configureStore from "./hooks-store/products-store";
 
+configureStore();
 ReactDOM.render(
-  <ProductsProvider>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </ProductsProvider>,
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
   document.getElementById("root")
 );
 // const root = ReactDOM.createRoot(document.getElementById('root'));root.render(<App />);
